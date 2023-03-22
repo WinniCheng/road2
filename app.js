08ce5efba2cb68b8f4440ba49dc9d0b6ef036ec3
WinniCheng <125950164+WinniCheng@users.noreply.github.com>
2023-03-23 00:55:11 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function showEnglish() {
+    document.getElementById("engVer").style.display = "";
+    document.getElementById("chiVer").style.display = "none";
+}
+
+function showChinese() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "";
+}
